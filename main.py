d2f8294beef6d33520c4a742a628a83502b6f3cc
marin <marincyrill@yandex.ru>
2022-03-15 16:46:59 +0500

    commit1

--- main.py
+++ main.py
@@ -1,33 +1,14 @@
-import os
-import sys
+from flask import Flask
+from flask_ngrok import run_with_ngrok
 
-import pygame
-import requests
-
-map_request = "https://static-maps.yandex.ru/1.x/?ll=37.385534,55.584227&spn=1,1&pt=37.439194,55.81782,pm,wt,s&l=map"
-response = requests.get(map_request)
-
-if not response:
-    print("Ошибка выполнения запроса:")
-    print(map_request)
-    print("Http статус:", response.status_code, "(", response.reason, ")")
-    sys.exit(1)
+app = Flask(__name__)
+run_with_ngrok(app)
 
 
-map_file = "map.png"
-with open(map_file, "wb") as file:
-    file.write(response.content)
+@app.route("/")
+def index():
+    return "Привет от приложения Flask"
 
 
-pygame.init()
-screen = pygame.display.set_mode((600, 450))
-
-screen.blit(pygame.image.load(map_file), (0, 0))
-
-pygame.display.flip()
-while pygame.event.wait().type != pygame.QUIT:
-    pass
-pygame.quit()
-
-
-os.remove(map_file)
+if __name__ == '__main__':
+    app.run()
